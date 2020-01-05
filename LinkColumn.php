d60a911c2cf1f3f70510aff76e4f6222656ee737
denis909 <denis909@mail.ru>
2020-01-05 14:01:01 +0300

    n/a

---
+++ LinkColumn.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace denis909\yii;
+
+use yii\helpers\Html;
+use Closure;
+
+class LinkColumn extends DataColumn
+{
+
+	public $contentOptions = [
+		'style' => 'width: 1%; white-space: nowrap;'
+	];
+
+	public $headerOptions = [
+		'style' => 'width: 1%;'
+	];
+
+	public $linkLabel;
+
+	public $url;
+
+	public $linkOptions = [];
+
+    protected function renderDataCellContent($model, $key, $index)
+    {
+    	if ($this->url instanceof Closure)
+    	{
+    		$url = call_user_func($this->url, $model, $key, $index);
+    	}
+    	else
+    	{
+            $url = $model->{$this->url};
+    	}    	
+
+    	return Html::a($this->linkLabel, $url, $this->linkOptions);
+    }
+
+}
